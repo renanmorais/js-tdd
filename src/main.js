6bf844a7be8e9219f54c3ca27d31b870aaac4835
Renan Morais <renanc.morais@gmail.com>
2020-07-11 23:34:58 -0300

    caso de teste fizzbuzz

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const FizzBuzz = (num) => {
+  if (num === 0) return 0;
   if (num % 3 === 0 && num % 5 === 0) {
     return 'FizzBuzz';
   }
@@ -10,5 +11,17 @@ const FizzBuzz = (num) => {
   }
   return num;
 };
+
+function foo() {
+  return 'bla';
+}
+
+function foo2() {
+  return 'bla';
+}
+
+function foo3() {
+  return 'bla';
+}
 
 export default FizzBuzz;
--- tests/main3.spec.js
+++ tests/main3.spec.js
@@ -30,4 +30,8 @@ describe('Main', () => {
   it('should return the number when non-multiple', () => {
     expect(FizzBuzz(7)).to.be.equal(7);
   });
+
+  it('should return 0 when 0', () => {
+    expect(FizzBuzz(0)).to.be.equal(0);
+  });
 });
